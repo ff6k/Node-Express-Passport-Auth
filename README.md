f02088ab38c57dc3a41a286b614f2fed574c2b7d
DESKTOP-JP1MEGQ\Wang <brzden32@gmail.com>
2021-07-24 02:57:56 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# node-express-mongoose-passport-jwt-rest-api-auth
+
+This source code is part of tutorials [Node, Express, Mongoose and Passport.js REST API Authentication](https://www.djamware.com/post/58eba06380aca72673af8500/node-express-mongoose-and-passportjs-rest-api-authentication)
+
+Before run this example, make sure you have installed and running MongoDB.
+
+Step to run locally:
+
+* Clone this repo
+* Run 'npm install'
+* Run 'npm start'
+
+If you think this source code is useful, it will be great if you just give it star or just buy me a cup of cofee [![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=Q5WK24UVWUGBN)
---
+++ app.js
@@ -0,0 +1,73 @@
+let express = require('express'),
+    path = require('path'),
+    cors = require('cors'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    config = require('./config/database');
+
+require('./config/passport')(passport);
+mongoose.connect(config.database, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true });
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(cors());
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+app.use(passport.initialize());
+
+
+/**
+ * Server Routes
+ */
+
+app.get('/', function(req, res) {
+    res.send('Page under construction.');
+});
+
+// Calling routes
+let authentication = require('./routes/auth');
+let book = require('./routes/book');
+
+// Using routes
+app.use('/api', authentication);
+app.use('/api', passport.authenticate('jwt', { session: false}), book);
+
+/**
+ * End Server Routes
+ */
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
